eccf0a9ee70d7ab28ce8f9ff30b852bae6e0ace1
Anatoliy <anartius@gmail.com>
2022-04-21 19:37:46 +0200

    Finish of the stage 1/6

--- Tasklist/task/src/tasklist/Main.kt
+++ Tasklist/task/src/tasklist/Main.kt
@@ -1,7 +1,35 @@
 package tasklist
 
-fun main() {
-    // write your code here
+fun main(){
+    val tasks = mutableListOf<String>()
+    tasks.addAll(getTasks())
+    if (tasks.isEmpty()) {
+        println("No tasks have been input")
+    } else  printTasks(tasks)
 }
 
 
+fun getTasks(): MutableList<String> {
+    val tasks = mutableListOf<String>()
+    val gaps = "\\s*".toRegex()
+
+    println("Input the tasks (enter a blank line to end):")
+    while(true) {
+        val input = readLine()!!
+            .replaceFirst(gaps, "").reversed()
+            .replaceFirst(gaps, "").reversed()
+        tasks.add(input)
+        if (tasks.last().isEmpty()) {
+            tasks.removeLast()
+            break
+        }
+    }
+    return tasks
+}
+
+
+fun printTasks(tasks: MutableList<String>) {
+    for (i in tasks.indices) {
+        println("${i + 1}" + (if(i < 9) "  " else " ") + tasks[i])
+    }
+}
