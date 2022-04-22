57dc8bbd0b827d972d50e131584a6d2c3109325b
Anatoliy <anartius@gmail.com>
2022-04-22 22:22:05 +0200

    Finish of the stage 2/6

--- Tasklist/task/src/tasklist/Main.kt
+++ Tasklist/task/src/tasklist/Main.kt
@@ -1,35 +1,58 @@
 package tasklist
 
 fun main(){
-    val tasks = mutableListOf<String>()
-    tasks.addAll(getTasks())
-    if (tasks.isEmpty()) {
-        println("No tasks have been input")
-    } else  printTasks(tasks)
-}
+    val tasks = mutableListOf(mutableListOf<String>())
+    tasks.clear()
 
+    while (true) {
+        println("Input an action (add, print, end):")
+        when (readln().lowercase()) {
+            "end" -> {
+                println("Tasklist exiting!")
+                return
+            }
+            "add" -> {
+                val task = getTask()
+                if (task.isNotEmpty()) tasks.add(task)
+                continue
+            }
+            "print" -> {
+                printTasks(tasks)
+                continue
+            }
 
-fun getTasks(): MutableList<String> {
-    val tasks = mutableListOf<String>()
-    val gaps = "\\s*".toRegex()
-
-    println("Input the tasks (enter a blank line to end):")
-    while(true) {
-        val input = readLine()!!
-            .replaceFirst(gaps, "").reversed()
-            .replaceFirst(gaps, "").reversed()
-        tasks.add(input)
-        if (tasks.last().isEmpty()) {
-            tasks.removeLast()
-            break
+            else -> {
+                println("The input action is invalid")
+                continue
+            }
         }
     }
-    return tasks
 }
 
 
-fun printTasks(tasks: MutableList<String>) {
-    for (i in tasks.indices) {
-        println("${i + 1}" + (if(i < 9) "  " else " ") + tasks[i])
+fun getTask(): MutableList<String> {
+    val task = mutableListOf<String>()
+    task.clear()
+
+    println("Input a new task (enter a blank line to end):")
+    while(true) {
+        val input = readln().trim()
+        if (input.isEmpty()) {
+            if (task.isEmpty()) println("The task is blank")
+            return task
+        }
+        task.add(input)
+    }
+}
+
+
+fun printTasks(tasks: MutableList<MutableList<String>>) {
+    if (tasks.isEmpty()) {
+        println("No tasks have been input")
+    } else {
+        for (i in tasks.indices) {
+            println("${i + 1}".padEnd(3)
+                    + tasks[i].joinToString("\n   ") + "\n")
+        }
     }
 }
