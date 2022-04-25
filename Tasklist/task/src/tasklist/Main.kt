f7232b14cb127d1815d40f3585488283d558cc49
Anatoliy <anartius@gmail.com>
2022-04-25 13:05:18 +0200

    Finish of the stage 3/6

--- Tasklist/task/src/tasklist/Main.kt
+++ Tasklist/task/src/tasklist/Main.kt
@@ -1,7 +1,13 @@
 package tasklist
+import kotlinx.datetime.*
+import java.lang.IllegalArgumentException
+
+data class Task( val priority: String,
+                 val toDo: MutableList<String>,
+                 val dateTime: LocalDateTime)
 
 fun main(){
-    val tasks = mutableListOf(mutableListOf<String>())
+    val tasks = mutableListOf<Task>()
     tasks.clear()
 
     while (true) {
@@ -13,46 +19,98 @@ fun main(){
             }
             "add" -> {
                 val task = getTask()
-                if (task.isNotEmpty()) tasks.add(task)
-                continue
+                if (task.toDo.isNotEmpty()) tasks.add(task)
             }
-            "print" -> {
-                printTasks(tasks)
-                continue
-            }
+            "print" -> printTasks(tasks)
 
-            else -> {
-                println("The input action is invalid")
-                continue
-            }
+            else -> println("The input action is invalid")
         }
     }
 }
 
 
-fun getTask(): MutableList<String> {
+fun getTask(): Task {
+
     val task = mutableListOf<String>()
     task.clear()
+    var priority: String
+
+    while (true) {
+        println("Input the task priority (C, H, N, L):")
+        priority = readln().uppercase()
+        if ("[CHNL]".toRegex().matches(priority)) break else continue
+    }
+
+    val date = getDate()
+    val dateTime = getDateTime(date)
 
     println("Input a new task (enter a blank line to end):")
     while(true) {
         val input = readln().trim()
         if (input.isEmpty()) {
             if (task.isEmpty()) println("The task is blank")
-            return task
+            break
         }
         task.add(input)
     }
+
+    return Task(priority, task, dateTime)
 }
 
 
-fun printTasks(tasks: MutableList<MutableList<String>>) {
+fun getDate(): LocalDate {
+    var date: LocalDate
+
+    while (true) {
+        println("Input the date (yyyy-mm-dd):")
+
+        try {
+
+            val input = mutableListOf<String>()
+            readln().split("-").toList()
+                .forEach { input.add(if (it.length < 2) "0$it" else it) }
+
+            date = input.joinToString("-").toLocalDate()
+            break
+        } catch (e: IllegalArgumentException) {
+            println("The input date is invalid")
+            continue
+        }
+    }
+    return date
+}
+
+
+fun getDateTime(date: LocalDate): LocalDateTime {
+    var dateTime: LocalDateTime
+
+    while (true) {
+        println("Input the time (hh:mm):")
+        try {
+            val input = mutableListOf<String>()
+            readln().split(":").toList()
+                .forEach { input.add(if (it.length < 2) "0$it" else it) }
+
+            dateTime = ("${date}T" + input.joinToString(":")).toLocalDateTime()
+            break
+        } catch (e: IllegalArgumentException) {
+            println("The input time is invalid")
+            continue
+        }
+    }
+    return dateTime
+}
+
+
+fun printTasks(tasks: MutableList<Task>) {
     if (tasks.isEmpty()) {
         println("No tasks have been input")
     } else {
         for (i in tasks.indices) {
-            println("${i + 1}".padEnd(3)
-                    + tasks[i].joinToString("\n   ") + "\n")
+            println("${i + 1}".padEnd(3) +
+                    tasks[i].dateTime.toString().replace('T', ' ') +
+                    " ${tasks[i].priority}\n" +
+                    tasks[i].toDo.joinToString("\n   ", "   ", "\n"))
         }
     }
 }
